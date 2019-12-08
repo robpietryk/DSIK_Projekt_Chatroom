a0e55e141afc6d9e63c4252df48599af1e0d75d9
robpietryk <56159119+robpietryk@users.noreply.github.com>
2019-12-08 20:27:28 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,3 @@
-//import socket
-//import select
-//import errno
-
-//lol include wszystko
 #include <stdio.h>
 #include <netdb.h>
 #include <netinet/in.h>
@@ -11,193 +6,86 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
-
-//HEADER_LENGTH = 10
-/*int HEADER_LENGTH = 1024;*/
-
-//IP = "150.254.79.22"
-char IP[] = "150.254.79.110";
-//char IP[] = "127.0.0.1";
-
-
-
-//PORT = 1234
-int PORT = 1234;
-//char HOST[] = "pc24e";
+#define BUFFER_SIZE 1024
+#define PORT 1234
 
 int main(void)
 {
-    struct sockaddr_in adr;
-
-    struct hostent *he;
-
+    char rec_mes[BUFFER_SIZE];
     char HOST[128];
-    printf("Podaj hostname servera.\n");
+    struct sockaddr_in adr;
+    struct hostent *he;
+    int received, client_socket;
+
+    printf("Provide server's hostname.\n");
     fflush(stdout);
-    fgets(HOST, 129, stdin);
+    fgets(HOST, 128, stdin);
     strtok(HOST, " \n\t\v\r");
-
-    he = gethostbyname(HOST); //polaczenie po hoscie
-
-    if (he == NULL) {
-            printf("Nieznany host\n");
+    he = gethostbyname(HOST);
+        if (he == NULL)
+        {
+            printf("Unknown host\n");
             return 0;
         }
 
-    int client_socket = socket(PF_INET, SOCK_STREAM, 0); //PRZYPISANIE SOCKETU
+    client_socket = socket(PF_INET, SOCK_STREAM, 0);
 
-     adr.sin_family = AF_INET;
-     adr.sin_port = htons(PORT);
-     adr.sin_addr = *(struct in_addr*) he->h_addr; //przy polaczeniu po hoscie
-     //adr.sin_addr.s_addr = inet_addr(IP);
+    adr.sin_family = AF_INET;
+    adr.sin_port = htons(PORT);
+    adr.sin_addr = *(struct in_addr*) he->h_addr;
 
-    if(connect(client_socket, (struct sockaddr*) &adr, sizeof(adr)) < 0) //POLACZENIE NR1
+    if(connect(client_socket, (struct sockaddr*) &adr, sizeof(adr)) < 0)
     {
-        printf("Polaczenie nie powiodlo sie\n");
+        printf("Cannot connect.\n");
         return 1;
     }
-    printf("Polaczenie nawiazane.\n");
+    printf("Connection established.\n");
 
-    printf("Podaj nazwe uzytkownika:\n");
+    printf("Provide username: ");
 
-    fflush(stdout); //fgetc(stdin);
+    fflush(stdout);
 
-    char username[1024];
-    fgets(username, 1025, stdin);
+    char username[BUFFER_SIZE];
 
-    printf("Username: %s\n", username);
+    fgets(username, (BUFFER_SIZE + 1), stdin);
 
     strtok(username, "\n");
 
-    printf("Username: %s\n", username);
-
     send(client_socket, username, strlen(username), 0);
 
-    //fgetc(stdin);
-
-    char wiadomosc[1024], dest[1024];
+    char wiadomosc[BUFFER_SIZE], dest[BUFFER_SIZE];
     while(1)
     {
-
-        printf("Podaj wiadomosc: \n");
+        printf("%s: ", username);
         fflush(stdout);
-         fgets(wiadomosc, 1025, stdin);
-         printf("TEST: %s\n", wiadomosc);
-         /*if(strlen(wiadomosc) == 0)
-         {
-             printf("DUPA\n");
-             continue;
-         }
-        else
-        {
-            char *temp, delim[] = " \n\t\v\r";
-         strcpy(dest, wiadomosc);
-        temp = strtok(dest, delim);
+        fgets(wiadomosc, BUFFER_SIZE, stdin);
 
-         if(strcmp(temp, "/stop") == 0)
+            char temp[BUFFER_SIZE], delim[] = " \n";
+            strcpy(dest, wiadomosc);
+            strtok(dest, delim);
+
+            if(strcmp(dest, "/stop") == 0 || strcmp(dest, "/exit") == 0 || strcmp(dest, "/disconnect") == 0 || strcmp(dest, "/wyjdz") == 0 || strcmp(dest, "/rozlacz") == 0 || strcmp(dest, "/quit") == 0)
             {
-                printf("TAK\n");
+                printf("Disconnected\n");
                 break;
-            }*/
-            //printf("BFRSEND\n");
+            }
             send(client_socket, wiadomosc, strlen(wiadomosc), 0);
-            printf("Wiadomosc wyslana.\n");
-            //printf("KONIEC\n");
-        //}
-
+            memset(rec_mes, '\0', BUFFER_SIZE);
+            while(1)
+            {
+                if(!recv(client_socket, rec_mes, BUFFER_SIZE, 0))
+                {
+                    printf("Server has closed the connection\n");
+                    exit(1);
+                }
+                else if(/*strlen(rec_mes) == 0 || */strcmp(rec_mes, "\0") == 0/* || rec_mes == 0*/) break;
+                else
+                {
+                    printf("%s", rec_mes);
+                }
+            }
     }
-
-    /*
-
-    //my_username = input("Username: ")
-    char my_username[128];
-    printf("%s", "Username: ");
-    gets(my_username);
-
-
-
-    //client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
-    //client_socket.connect((IP, PORT))
-
-    printf("Polaczono");
-    */
-
-
-
-
-
-
     close(client_socket);
 
     return 0;
 }
-
-/*
-
-# Set connection to non-blocking state, so .recv() call won;t block, just return some exception we'll handle
-client_socket.setblocking(False)
-
-# Prepare username and header and send them
-# We need to encode username to bytes, then count number of bytes and prepare header of fixed size, that we encode to bytes as well
-username = my_username.encode('utf-8')
-username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
-client_socket.send(username_header + username)
-
-while True:
-
-    # Wait for user to input a message
-    message = input(f'{my_username} > ')
-
-    # If message is not empty - send it
-    if message:
-
-        # Encode message to bytes, prepare header and convert to bytes, like for username above, then send
-        message = message.encode('utf-8')
-        message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
-        client_socket.send(message_header + message)
-
-    try:
-        # Now we want to loop over received messages (there might be more than one) and print them
-        while True:
-
-            # Receive our "header" containing username length, it's size is defined and constant
-            username_header = client_socket.recv(HEADER_LENGTH)
-
-            # If we received no data, server gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
-            if not len(username_header):
-                print('Connection closed by the server')
-                sys.exit()
-
-            # Convert header to int value
-            username_length = int(username_header.decode('utf-8').strip())
-
-            # Receive and decode username
-            username = client_socket.recv(username_length).decode('utf-8')
-
-            # Now do the same for message (as we received username, we received whole message, there's no need to check if it has any length)
-            message_header = client_socket.recv(HEADER_LENGTH)
-            message_length = int(message_header.decode('utf-8').strip())
-            message = client_socket.recv(message_length).decode('utf-8')
-
-            # Print message
-            print(f'{username} > {message}')
-
-    except IOError as e:
-        # This is normal on non blocking connections - when there are no incoming data error is going to be raised
-        # Some operating systems will indicate that using AGAIN, and some using WOULDBLOCK error code
-        # We are going to check for both - if one of them - that's expected, means no incoming data, continue as normal
-        # If we got different error code - something happened
-        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-            print('Reading error: {}'.format(str(e)))
-            sys.exit()
-
-        # We just did not receive anything
-        continue
-
-    except Exception as e:
-        # Any other exception - something happened, exit
-        print('Reading error: '.format(str(e)))
-        sys.exit()
-        */
